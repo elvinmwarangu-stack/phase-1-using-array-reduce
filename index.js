e4f0900beb653d20632870b2fe6cf5c230004010
elvinmwarangu-stack <elvin.mwarangu@student.moringaschool.com>
2025-09-15 08:04:26 +0300

    commit a file

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
 
 // Code your solution here
+const batteryBatches = [
+  { assembled: 5 },
+  { assembled: 10 },
+  { assembled: 6 },
+  { assembled: 10 }
+];
+
+const totalBatteries = batteryBatches.reduce((total, batch) => {
+  return total + batch.assembled;
+}, 0);
+
+module.exports = {
+  totalBatteries
+};
